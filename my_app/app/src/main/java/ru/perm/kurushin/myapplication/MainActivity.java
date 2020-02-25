59297860899861a8dc63d755214d38a0d7c0145a
daniel-kurushin <daniel.kurushin@gmail.com>
2020-02-25 10:14:16 +0500

    uuppss

--- my_app/app/src/main/java/ru/perm/kurushin/myapplication/MainActivity.java
+++ my_app/app/src/main/java/ru/perm/kurushin/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package ru.perm.kurushin.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 // Привет!!!
 // и тебе привет!
+// а тебе - нет!
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
